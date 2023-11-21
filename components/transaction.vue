85b51cff2598bc12ecc41e97f3168898d00c13ea
Rafael Dias <rafael.ddsociety@gmail.com>
2023-11-21 07:28:26 -0300

    transaction

--- components/transaction.vue
+++ components/transaction.vue
@@ -1,9 +1,25 @@
 <script setup>
   const { currency } = useCurrency(3000)
+  const items = [
+    [
+      {
+        label: 'Editar',
+        icon: 'i-heroicons-pencil-square-20-solid',
+        click: () => console.log('Editado'),
+      },
+      {
+        label: 'Delete',
+        icon: 'i-heroicons-trash-20-solid',
+        click: () => console.log('Excluido'),
+      },
+    ],
+  ]
 </script>
 
 <template>
-  <div class="grid grid-cols-2 py-4 border-b">
+  <div
+    class="grid grid-cols-2 py-4 border-b border-gray-200 dark:border-gray-800 text-gray-900 dark:text-gray-100"
+  >
     <div class="flex items-center justify-between">
       <div class="flex items-center space-x-1">
         <UIcon name="i-heroicons-arrow-up-right" class="text-green-600" />
@@ -16,7 +32,15 @@
 
     <div class="flex items-center justify-end space-x-2">
       <div>{{ currency }}</div>
-      <div><button>Opções</button></div>
+      <div>
+        <UDropdown :items="items" :popper="{ placement: 'bottom-end' }">
+          <UButton
+            color="white"
+            variant="ghost"
+            trailing-icon="i-heroicons-ellipsis-horizontal"
+          />
+        </UDropdown>
+      </div>
     </div>
   </div>
 </template>
